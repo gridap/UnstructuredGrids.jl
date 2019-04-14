054c52de9e3ecb7abb1ae26ab325c6a7b9098e11
fverdugo <fverdugo@cimne.upc.edu>
2019-04-14 12:40:54 +0200

    Added face_to_cells

--- src/UnstructuredGrids.jl
+++ src/UnstructuredGrids.jl
@@ -1,5 +1,98 @@
 module UnstructuredGrids
 
-greet() = print("Hello World!")
+export face_to_cells
+export rewind_ptrs!
+export length_to_ptrs!
+
+# Functionality given by this module
+
+"""
+Given the faces on the boundary of each cell,
+find the cells around each face.
+"""
+function face_to_cells(
+  cell_to_faces_data::AbstractVector{<:Integer},
+  cell_to_faces_ptrs::AbstractVector{<:Integer},
+  nfaces = maximum(cell_to_faces_data))
+  _face_to_cells(cell_to_faces_data,cell_to_faces_ptrs,nfaces)
+end
+
+"""
+Rewind the given vector.
+"""
+function rewind_ptrs!(ptrs::AbstractVector{<:Integer})
+  @inbounds for i in (length(ptrs)-1):-1:1
+    ptrs[i+1] = ptrs[i]
+  end
+  ptrs[1] = 1
+end
+
+"""
+Given a vector of integers, mutate it from length state to pointer state.
+"""
+function length_to_ptrs!(ptrs::AbstractArray{<:Integer})
+  ptrs[1] = 1
+  @inbounds for i in 1:(length(ptrs)-1)
+    ptrs[i+1] += ptrs[i]
+  end
+end
+
+# Helpers
+
+function _face_to_cells(cell_to_faces_data, cell_to_faces_ptrs, nfaces)
+
+  face_to_cells_ptrs = zeros(Int,nfaces+1)
+
+  _face_to_cells_count!(
+    face_to_cells_ptrs, cell_to_faces_data, cell_to_faces_ptrs)
+
+  length_to_ptrs!(face_to_cells_ptrs)
+
+  ndata = face_to_cells_ptrs[end]-1
+
+  face_to_cells_data = Vector{Int}(undef,ndata)
+
+  _face_to_cells_fill!(
+    face_to_cells_data, face_to_cells_ptrs,
+    cell_to_faces_data, cell_to_faces_ptrs)
+
+  rewind_ptrs!(face_to_cells_ptrs)
+
+  (face_to_cells_data, face_to_cells_ptrs)
+
+end
+
+function _face_to_cells_count!(
+  face_to_cells_ptrs, cell_to_faces_data, cell_to_faces_ptrs)
+
+  ncells = length(cell_to_faces_ptrs) - 1
+  for cell in 1:ncells
+    a = cell_to_faces_ptrs[cell]
+    b = cell_to_faces_ptrs[cell+1]-1
+    for p in a:b
+      face = cell_to_faces_data[p]
+      face_to_cells_ptrs[face+1] += 1
+    end
+  end
+
+end
+
+function _face_to_cells_fill!(
+  face_to_cells_data, face_to_cells_ptrs,
+  cell_to_faces_data, cell_to_faces_ptrs)
+
+  ncells = length(cell_to_faces_ptrs) - 1
+  for cell in 1:ncells
+    a = cell_to_faces_ptrs[cell]
+    b = cell_to_faces_ptrs[cell+1]-1
+    for p in a:b
+      face = cell_to_faces_data[p]
+      q = face_to_cells_ptrs[face]
+      face_to_cells_data[q] = cell
+      face_to_cells_ptrs[face] += 1
+    end
+  end
+
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
+module UnstructuredGridsTests
+
 using UnstructuredGrids
 using Test
 
 @testset "UnstructuredGrids.jl" begin
-    # Write your own tests here.
+
+  a = [9,2,1,2,4,7,4]
+  b = [1,9,2,1,2,4,7]
+
+  rewind_ptrs!(a)
+  @test a == b
+
+  a = [3,2,4,2]
+  b = [1,3,7,9]
+
+  length_to_ptrs!(a)
+  @test a == b
+
+  include("Mock2D.jl")
+
+  _vertex_to_cells_data, _vertex_to_cells_ptrs = face_to_cells(
+    cell_to_vertices_data,cell_to_vertices_ptrs)
+
+  @test _vertex_to_cells_data == vertex_to_cells_data
+  @test _vertex_to_cells_ptrs == vertex_to_cells_ptrs
+
+
 end
+
+end #module UnstructuredGridsTests
