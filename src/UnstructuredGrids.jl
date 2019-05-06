58a974b2a81043cd3750ca60344cb6464199c0e8
fverdugo <fverdugo@cimne.upc.edu>
2019-05-06 14:32:19 +0200

    Exporting mode names at the top level

--- src/UnstructuredGrids.jl
+++ src/UnstructuredGrids.jl
@@ -7,21 +7,27 @@ include("Factories.jl")
 include("VTK.jl")
 
 export Connections
+export RefCell
 export UGrid
+export VERTEX
 export connections
 export coordinates
 export writevtk
 export list, ptrs
 export generate_dual_connections
 export generate_cell_to_faces
+export find_cell_to_faces
 
 using UnstructuredGrids.Core: Connections
 using UnstructuredGrids.Core: UGrid
+using UnstructuredGrids.Core: RefCell
 using UnstructuredGrids.Core: list, ptrs
 using UnstructuredGrids.Core: connections
 using UnstructuredGrids.Core: coordinates
+using UnstructuredGrids.Core: VERTEX
 using UnstructuredGrids.VTK: writevtk
 using UnstructuredGrids.Kernels: generate_dual_connections
 using UnstructuredGrids.Kernels: generate_cell_to_faces
+using UnstructuredGrids.Kernels: find_cell_to_faces
 
 end # module UnstructuredGrids
